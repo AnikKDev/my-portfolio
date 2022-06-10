ab50452b723f1355a659b4b3d10ae5ae3bcdc1ce
AnikKDev <anikdev2016@gmail.com>
2022-06-10 21:28:21 +0600

    added images for details section in json file

--- src/Pages/ProjectDetail/Project.js
+++ src/Pages/ProjectDetail/Project.js
@@ -2,15 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 const Project = () => {
     const { id } = useParams();
-    const [project, setProject] = useState([]);
+    const [project, setProject] = useState({});
     useEffect(() => {
-        fetch('projects.json')
+        fetch('https://raw.githubusercontent.com/AnikKDev/my-portfolio/main/public/projects.json')
             .then(res => res.json())
-            .then(data => console.log(data))
-    }, [])
+            .then(data => {
+                // setProject(data);
+                const foundProject = data.find(a => a.id == id)
+                setProject(foundProject);
+            })
+    }, [id, project])
     return (
         <div>
-            {id}
+
         </div>
     );
 };
